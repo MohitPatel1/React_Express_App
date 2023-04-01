23dec6370367ba06e1a80aa5a61009029384b8ce
Mohit <mohit@mohit.s-pc>
2023-04-01 10:48:46 +0530

    added tailwind not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
 
 class App extends Component {
     constructor(props) {
@@ -21,12 +20,9 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
+            <div>
+                <p className="text-3xl font-bold underline bg-green-500">Madarchod</p>
+                <p>{this.state.apiResponse}</p>
             </div>
         );
     }
